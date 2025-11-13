43d6aaf457f7133dc576ff1ea93ca1f7390cb004
Thang Do <csessh@hey.com>
2025-11-13 13:48:58 +0700

    feat: implement GameController state machine
    Add game flow orchestration with state transitions

---
+++ phom/controllers/game_controller.lua
@@ -0,0 +1,122 @@
+local Constants = require("utils/constants")
+local GameState = require("models/game_state")
+
+local GameController = {}
+GameController.__index = GameController
+
+function GameController.new()
+  local instance = {
+    game_state = GameState.new(),
+    animation_queue = {}
+  }
+  return setmetatable(instance, GameController)
+end
+
+function GameController:update(dt)
+  -- Handle state machine
+  if self.game_state.current_state == Constants.STATES.MENU then
+    self:handleMenu()
+  elseif self.game_state.current_state == Constants.STATES.DEALING then
+    self:handleDealing()
+  elseif self.game_state.current_state == Constants.STATES.PLAYER_TURN then
+    self:handlePlayerTurn()
+  elseif self.game_state.current_state == Constants.STATES.AI_TURN then
+    self:handleAITurn()
+  elseif self.game_state.current_state == Constants.STATES.ROUND_END then
+    self:handleRoundEnd()
+  elseif self.game_state.current_state == Constants.STATES.GAME_OVER then
+    self:handleGameOver()
+  end
+end
+
+function GameController:handleMenu()
+  -- Auto-start for now
+  self:startNewRound()
+end
+
+function GameController:startNewRound()
+  self.game_state = GameState.new()
+  self.game_state.current_state = Constants.STATES.DEALING
+end
+
+function GameController:handleDealing()
+  -- Deal cards (no animation for now)
+  self.game_state:dealCards(9)
+
+  -- Add first card to discard pile
+  local first_discard = self.game_state.deck:draw()
+  if first_discard then
+    self.game_state:addToDiscard(first_discard)
+  end
+
+  -- Move to first player turn
+  self.game_state.current_state = Constants.STATES.PLAYER_TURN
+  self.game_state.turn_substep = Constants.TURN_SUBSTEPS.CHOOSE_ACTION
+end
+
+function GameController:handlePlayerTurn()
+  -- Input handled by InputController
+  -- Just manage substeps here
+end
+
+function GameController:handleAITurn()
+  -- AI logic will be handled by AIController in Task 13
+  -- For now, this is empty as per task instructions
+end
+
+function GameController:handleRoundEnd()
+  self.game_state:calculateAllScores()
+  print("Round ended!")
+  for _, player in ipairs(self.game_state.players) do
+    print("Player " .. player.id .. " score:", self.game_state.scores[player.id])
+  end
+  self.game_state.current_state = Constants.STATES.GAME_OVER
+end
+
+function GameController:handleGameOver()
+  -- Wait for restart
+end
+
+function GameController:drawCard()
+  local card = self.game_state.deck:draw()
+  if card then
+    self.game_state:getCurrentPlayer():addCardToHand(card)
+    self.game_state.turn_substep = Constants.TURN_SUBSTEPS.DISCARD_PHASE
+  end
+end
+
+function GameController:discardCard(card)
+  local current_player = self.game_state:getCurrentPlayer()
+  if current_player:removeCardFromHand(card) then
+    self.game_state:addToDiscard(card)
+    self:endTurn()
+  end
+end
+
+function GameController:endTurn()
+  -- Check win condition
+  local winner = self.game_state:checkWinCondition()
+  if winner then
+    self.game_state.current_state = Constants.STATES.ROUND_END
+    return
+  end
+
+  -- Check if deck empty
+  if self.game_state:isDeckEmpty() then
+    self.game_state.current_state = Constants.STATES.ROUND_END
+    return
+  end
+
+  -- Next player
+  self.game_state:nextPlayer()
+
+  local next_player = self.game_state:getCurrentPlayer()
+  if next_player.type == "human" then
+    self.game_state.current_state = Constants.STATES.PLAYER_TURN
+    self.game_state.turn_substep = Constants.TURN_SUBSTEPS.CHOOSE_ACTION
+  else
+    self.game_state.current_state = Constants.STATES.AI_TURN
+  end
+end
+
+return GameController
--- phom/main.lua
+++ phom/main.lua
@@ -1,23 +1,22 @@
 local flux = require("libraries/flux")
-local GameState = require("models/game_state")
+local GameController = require("controllers/game_controller")
 local GameView = require("views/game_view")
 
-local game_state
+local game_controller
 local game_view
 
 function love.load()
   love.graphics.setDefaultFilter("nearest", "nearest")
 
-  game_state = GameState.new()
-  game_state:dealCards(9)
-
+  game_controller = GameController.new()
   game_view = GameView.new()
 end
 
 function love.update(dt)
   flux.update(dt)
+  game_controller:update(dt)
 end
 
 function love.draw()
-  game_view:draw(game_state)
+  game_view:draw(game_controller.game_state)
 end
